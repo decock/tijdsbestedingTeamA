d615c42567c424204ecb83b399c4e4a88be581a4
mrdee72@gmail.com <mrdee72@gmail.com>
2013-09-07 01:51:36 +0200

    opkuis1

--- tijdsbesteding/src/be/teama/tijdsbesteding/client/Tijdsbesteding.java
+++ tijdsbesteding/src/be/teama/tijdsbesteding/client/Tijdsbesteding.java
@@ -17,17 +17,7 @@ import com.google.gwt.user.client.ui.RootPanel;
 import com.google.gwt.user.client.ui.TextBox;
 import com.google.gwt.user.client.ui.VerticalPanel;
 
-/**
- * Entry point classes define <code>onModuleLoad()</code>.
- */
 public class Tijdsbesteding implements EntryPoint {
-	/**
-	 * The message displayed to the user when the server cannot be reached or
-	 * returns an error.
-	 */
-	private static final String SERVER_ERROR = "An error occurred while "
-			+ "attempting to contact the server. Please check your network "
-			+ "connection and try again.";
 
 	/**
 	 * Create a remote service proxy to talk to the server-side Greeting service.
@@ -127,7 +117,6 @@ public class Tijdsbesteding implements EntryPoint {
 										.setText("Remote Procedure Call - Failure");
 								serverResponseLabel
 										.addStyleName("serverResponseLabelError");
-								serverResponseLabel.setHTML(SERVER_ERROR);
 								dialogBox.center();
 								closeButton.setFocus(true);
 							}
